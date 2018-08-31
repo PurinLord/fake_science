afafe81a8a08dc5ae33b1edb5ef7576cb566ab0b
Victor <purin@tutanota.de>
2018-08-30 20:08:03 -0500

    non global vesion

--- create_corpus/news/news/spiders/crawl.py
+++ create_corpus/news/news/spiders/crawl.py
@@ -2,6 +2,7 @@ import scrapy
 import re
 import os
 
+
 class QuotesSpider(scrapy.Spider):
     name = "init"
 
@@ -9,27 +10,26 @@ class QuotesSpider(scrapy.Spider):
 
     def __init__(self):
         #self.start_url = 'https://yournewswire.com'
-        self.current_site = ''
         self.root = './extracted/'
-        self.filename = ''
-        self.visited = set()
-        self.self_contained = list()
-
-        self.news = list()
+        self.visited = dict()
 
 
-    def set_filename(self, current_site):
+    def create_filename(self, current_site):
         filename = re.search('http.*//(.*)\..*', current_site).group(1)
         filename += '.txt'
         return filename
 
-
     
-    def add_site_scope(self, response):
-        r = response.css("a::attr(href)").extract()
-        new_url = {url for url in r if url.startswith(self.current_site)}
-        missing = new_url.difference(set(self.self_contained))
-        self.self_contained.extend(missing)
+    def add_self_contained(self, response):
+        all_url = response.css("a::attr(href)").extract()
+
+        # keep only same domain sites
+        new_url = {url for url in all_url 
+                if url.startswith(response.meta['current_site'])}
+        ## remove visited
+        remaining_url = new_url - self.visited[response.meta['current_site']]
+
+        response.meta['self_contained'] = remaining_url
 
         #missing = self.self_contained.difference(set(self.visited))
 
@@ -48,51 +48,62 @@ class QuotesSpider(scrapy.Spider):
 
     def get_site(self, response):
         out_site = response.xpath('//p[contains(., "Source:")]/a/text()').extract_first()
-        self.current_site = out_site
-        self.filename = self.set_filename(self.current_site)
-        if os.path.exists(self.root+self.filename):
-            yield None
-        print('-- GET --', self.current_site)
-        yield scrapy.Request(
+
+        print('-- GET --', out_site)
+        self.visited[out_site] = set()
+
+        request = scrapy.Request(
                     out_site,
                     callback=self.crawl_site
                 )
+        request.meta['current_site'] = out_site 
+        request.meta['filename'] = self.create_filename(out_site)
+        # open file???
+        yield request
+        # Close file??
 
 
     def crawl_site(self, response):
-        self.visited = set()
-        self.self_contained = list()
-        # lista de todas las ligas
-        self.add_site_scope(response)
+        self.add_self_contained(response)
 
-        for url in self.self_contained:
+        for url in response.meta['self_contained']:
             print('-- CRAWL --', url)
-            if url not in self.visited:
-                self.visited.add(url)
-                yield scrapy.Request(
-                    url,
-                    callback=self.extract_news
-                )
-
+            self.visited[response.meta['current_site']].add(url)
+            request = scrapy.Request(
+                url,
+                callback=self.extract_news
+            )
+            request.meta['current_site'] = response.meta['current_site']
+            request.meta['filename'] = response.meta['filename']
+            yield request
     
     def extract_news(self, response):
         #self.log('%s' % response.url)
         # Buscar la alternancia de ligas contenidas y texto (imagenes)
-        self.add_site_scope(response)
         text = response.xpath('//*/text()').extract()
         clean = [e.strip() for e in text if self.is_valid(e)]
         joined = '\n'.join(clean)
         news = response.url + '\n' + joined + '\n\n'
-        print('-- NEWS --', self.root+self.filename)
-        with open(self.root+self.filename, 'a') as f:
-            f.write(news)
+        print('-- NEWS --')
+        #response.meta['file'].write(news)
+        f = open(self.root+request.meta['filename'], 'a')
+        f.write(news)
+        f.close()
+        
+        # Recursive call
+        request = scrapy.Request(
+                    response.url,
+                    callback=self.crawl_site
+                )
+        request.meta['current_site'] = response.meta['current_site']
+        request.meta['filename'] = response.meta['filename']
+        yield request
 
 
     def is_valid(self, text):
         text = text.strip()
         if text == '': return False
-        if text[-1] != '.': return False
-        # otros signos de puntuacion
+        if text[-1] not in {'.', '?', '!'}: return False
         if '{' in text: return False
         if '<' in text: return False
         return True
