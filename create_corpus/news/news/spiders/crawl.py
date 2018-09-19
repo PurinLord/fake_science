ca4d8597e7fa07f73ebee407497e6a5ad005041f
Victor Martinell <purin@tutanota.de>
2018-09-19 01:26:23 -0500

    extract ony domain url, get pseudo consp info, save in elastic

--- create_corpus/news/news/spiders/crawl.py
+++ create_corpus/news/news/spiders/crawl.py
@@ -2,6 +2,8 @@ import scrapy
 import re
 import os
 
+from ES.indexes import News
+
 
 class QuotesSpider(scrapy.Spider):
     name = "init"
@@ -10,18 +12,18 @@ class QuotesSpider(scrapy.Spider):
 
     def __init__(self):
         #self.start_url = 'https://yournewswire.com'
-        self.root = './extracted/'
         self.visited = dict()
 
 
-    def create_filename(self, current_site):
-        filename = re.search('http.*//(.*)\..*', current_site).group(1)
-        filename += '.txt'
-        return filename
+    def extract_site_name(self, current_site):
+        site_name = re.search(
+                'http.*//(?:www\.|)(.*)\..*', 
+                current_site).group(1)
+        return site_name
 
     
     def add_self_contained(self, response):
-        local_url = response.xpath('//a[starts-with(@href, "%s")]/@href' % (response.meta['current_site'])).extract()
+        local_url = response.xpath('//a[contains(@href, "%s")]/@href' % (response.meta['site_name'])).extract()
         relative_url = response.xpath('//a[starts-with(@href, "/")]/@href').extract()
 
         full_link = [
@@ -43,6 +45,7 @@ class QuotesSpider(scrapy.Spider):
         r = response.css("a::attr(href)").extract()
         all_sites = r[43:309]
         #all_sites = r[48:98]; print('-- ALL --', all_sites)
+
         for url in all_sites:
             yield scrapy.Request(
                 url,
@@ -54,6 +57,18 @@ class QuotesSpider(scrapy.Spider):
     def get_site(self, response):
         out_site = response.xpath('//p[contains(., "Source:")]/a/text()').extract_first()
 
+        # extrae valor de conspiracy y pseudoscience
+        # ['con1', 'pseudo5']
+        cons_pseud = response.xpath('//img/@data-image-title').extract()
+
+        # Factual reporting extract
+        factual = response.xpath('//p[contains(., "Factual Reporting")]/span/strong/text()').extract_first()
+
+        # Notes
+        notes = response.xpath('//p[contains(., "Notes:")]/text()').extract_first()
+        # Update
+        update = response.xpath('//p[contains(., "UPDATE:")]/text()').extract_first()
+
         print('-- GET --', out_site)
         self.visited[out_site] = set()
 
@@ -61,11 +76,14 @@ class QuotesSpider(scrapy.Spider):
                     out_site,
                     callback=self.crawl_site
                 )
-        request.meta['current_site'] = out_site 
-        request.meta['filename'] = self.create_filename(out_site)
-        # open file???
+        request.meta['current_site'] = out_site
+        request.meta['site_name'] = self.extract_site_name(out_site)
+        request.meta['cons'] = int(re.findall('\d+', cons_pseud[0])[0])
+        request.meta['preudo'] = int(re.findall('\d+', cons_pseud[1])[0])
+        request.meta['factual'] = factual
+        request.meta['notes'] = notes
+        request.meta['update'] = update
         yield request
-        # Close file??
 
 
     def crawl_site(self, response):
@@ -78,33 +96,33 @@ class QuotesSpider(scrapy.Spider):
                 url,
                 callback=self.extract_news
             )
-            request.meta['current_site'] = response.meta['current_site']
-            request.meta['filename'] = response.meta['filename']
+            request.meta.update(response.meta)
             yield request
     
     def extract_news(self, response):
-        #self.log('%s' % response.url)
-        # Buscar la alternancia de ligas contenidas y texto (imagenes)
-        alternating = response.xpath('//a[starts-with(@href, "%s")]/following::text()' % (response.meta['current_site'])).extract()
-        clean_alt = self.clean_text(alternating)
 
         text = response.xpath('//*/text()').extract()
         clean = self.clean_text(text)
-        joined = '\n'.join(clean)
-        news = response.url + '\n' + joined + '\n\n'
+        news = '\n'.join(clean)
         print('-- NEWS --')
-        #response.meta['file'].write(news)
-        f = open(self.root+response.meta['filename'], 'a')
-        f.write(news)
-        f.close()
-        
+
+        n = News()
+        n.source = response.meta['current_site'] 
+        n.url =  response.meta['site_name']
+        n.content = news
+        n.cons = response.meta['cons']
+        n.preudo = response.meta['preudo']
+        n.factual = response.meta['factual']
+        n.notes = response.meta['notes']
+        n.update = response.meta['update']
+        n.save()
+
         # Recursive call
         request = scrapy.Request(
                     response.url,
                     callback=self.crawl_site
                 )
-        request.meta['current_site'] = response.meta['current_site']
-        request.meta['filename'] = response.meta['filename']
+        request.meta.update(response.meta)
         yield request
 
 
@@ -116,7 +134,7 @@ class QuotesSpider(scrapy.Spider):
         text = text.strip()
         if text == '': return False
         if text[-1] not in {'.', '?', '!'}: return False
-        # ]
+
         if '{' in text: return False
         if '<' in text: return False
         return True
