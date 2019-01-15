2b0e2a71cfc721e50db04d1689f8b948f81c403a
victor <purin@tutanota.de>
2019-01-15 14:24:40 -0600

    script to make ulmfit training

--- pytorch_model.py
+++ pytorch_model.py
@@ -4,24 +4,27 @@
 from fastai import *
 from fastai.text import * 
 
-'''
-from dataset_creation import train_test_split, make_df
+from dataset_creation import train_test_split
 
 
-x_train, y_train, x_test, y_test = train_test_split(0.8, 0.8, labels=[1,5], seed=4985, encode=None)
+x_train, y_train, x_test, y_test = train_test_split(0.8, 0.8, labels=[1,5], seed=4985)
 
-train_df = make_df(x_train, y_train.astype(str), x_name='text', y_name='label')
+d = {'label': y_train.astype(str), 'text':x_train}
+train_df = DataFrame(data=d)
                                     
-test_df = make_df(x_test, y_test.astype(str), x_name='text', y_name='label')
+d = {'label': y_test.astype(str), 'text':x_test}
+test_df = DataFrame(data=d)
 
-# Language model data
-data_lm = TextLMDataBunch.from_df('.', train_df, test_df)
-'''
+path = 'lm_data'
 
-data_lm = TextLMDataBunch.load('lm_data', '')
+data_lm = TextLMDataBunch.from_df(path, train_df, test_df)
+data_clas = TextClasDataBunch.from_df(path, train_df, test_df, vocab=data_lm.train_ds.vocab, bs=32)
 
-# Classifier model data
-#data_clas = TextClasDataBunch.from_csv(path, 'texts.csv', vocab=data_lm.train_ds.vocab, bs=32)
+data_lm.save()
+data_clas.save()
+
+data_lm = TextLMDataBunch.load(path)
+data_clas = TextClasDataBunch.load(path, bs=32)
 
 learn = language_model_learner(data_lm, pretrained_model=URLs.WT103, drop_mult=0.5)
 learn.fit_one_cycle(1, 1e-2)
@@ -29,15 +32,24 @@ learn.fit_one_cycle(1, 1e-2)
 learn.unfreeze()
 learn.fit_one_cycle(1, 1e-3)
 
-learn.predict("This is a review about", n_words=10)
+learn.predict("I like the", n_words=10)
 
-#data_clas = TextClasDataBunch.from_df('.', train_df, test_df, vocab=data_lm.train_ds.vocab, bs=32)
-#learn = text_classifier_learner(data_clas, drop_mult=0.5)
-#learn.load_encoder('ft_enc')
-#learn.fit_one_cycle(1, 1e-2)
-#learn.freeze_to(-2)
-#learn.fit_one_cycle(1, slice(5e-3/2., 5e-3))
-#learn.unfreeze()
-#learn.fit_one_cycle(1, slice(2e-3/100, 2e-3))
-#learn.predict("Trump lies!")
+learn.save_encoder('ft_enc')
+
+learn = text_classifier_learner(data_clas, drop_mult=0.5)
+learn.load_encoder('ft_enc')
+
+data_clas.show_batch()
+
+learn.fit_one_cycle(1, 1e-2)
+
+learn.freeze_to(-2)
+learn.fit_one_cycle(1, slice(5e-3/2., 5e-3))
+
+learn.unfreeze()
+learn.fit_one_cycle(1, slice(2e-3/100, 2e-3))
+
+learn.predict("My screen does not load")
+
+learn.save('clas')
 
